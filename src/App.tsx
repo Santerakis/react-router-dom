f8d20cb381fb61bda69aefa93f259092c9dda796
Sergey <santerakis@gmail.com>
2022-12-09 20:54:49 +0200

    2lesson

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,27 @@ function App() {
         <div className="App">
             <NavLink to={'/'}>main</NavLink>...
             <NavLink to={'/login'}>login</NavLink>...
-            <NavLink to={'/profile'}>profile</NavLink>
+            <NavLink to={'/profile'}>profile</NavLink>...
+            <NavLink to={'/profile/settings'}>settings</NavLink>
 
             <Routes>
+                <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>MAIN</div>}/>
-                <Route path={'/login'} element={<div>LOGIN</div>}/>
-                <Route path={'/profile'} element={<div>PROFILE</div>}/>
+                <Route path={'/login/*'} element={<div>LOGIN</div>}/>
+                <Route path={'/profile/*'} element={<div>PROFILE</div>}/>
+                {/*<Route path={'/profile/settings'} element={<div>SETTINGS</div>}/>*/}
             </Routes>
+
+            {/*<NavLink to={'/'}>main</NavLink>...*/}
+            {/*<NavLink to={'/login'}>login</NavLink>...*/}
+            {/*<NavLink to={'/profile'}>profile</NavLink>*/}
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/'} element={<div>MAIN</div>}/>*/}
+            {/*    <Route path={'/login'} element={<div>LOGIN</div>}/>*/}
+            {/*    <Route path={'/profile'} element={<div>PROFILE</div>}/>*/}
+            {/*</Routes>*/}
+
         </div>
     );
 }
