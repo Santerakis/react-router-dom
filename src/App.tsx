70f4fe1434fdf778159b5f5e17fdf789e4d9f44c
Sergey <santerakis@gmail.com>
2022-12-10 14:17:48 +0200

    2.3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <NavLink to={'/'}>main</NavLink>...
+            <NavLink to={''}>main</NavLink>...
             <NavLink to={'/login'}>login</NavLink>...
             <NavLink to={'/profile'}>profile</NavLink>...
             <NavLink to={'/profile/settings'}>settings</NavLink>
@@ -20,9 +20,71 @@ function App() {
                         <Outlet/>
                     </div>
                 )}>
-                    <Route path={'/profile/settings'} element={<div>SETTINGS</div>}/>
+                    <Route path={'*'} element={<div>profile page not found</div>}/>
+                    <Route index element={<div>CHECK ID</div>}/> // если нет параметров
+                    <Route path={':id'} element={<div>-ID-</div>}/>
+                    <Route path={'settings'} element={<div>SETTINGS</div>}/>
                 </Route>
             </Routes>
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/*'} element={<div>404</div>}/>*/}
+            {/*    <Route path={'/'} element={<div>MAIN</div>}/>*/}
+            {/*    <Route path={'/login/*'} element={<div>LOGIN</div>}/>*/}
+            {/*    <Route path={'/profile'} element={(*/}
+            {/*        <div>*/}
+            {/*            PROFILE*/}
+            {/*            <Outlet/>*/}
+            {/*        </div>*/}
+            {/*    )}>*/}
+            {/*        <Route path={'*'} element={<div>profile page not found</div>}/> //надо 2 параметра после profile*/}
+            {/*        <Route path={':id'} element={<div>-ID-</div>}/> // на 1 п. после p.; ':id/:x' на 2п.*/}
+            {/*        <Route path={'settings'} element={<div>SETTINGS</div>}/>*/}
+            {/*    </Route>*/}
+            {/*</Routes>*/}
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/*'} element={<div>404</div>}/>*/}
+            {/*    <Route path={'/'} element={<div>MAIN</div>}/>*/}
+            {/*    <Route path={'/login/*'} element={<div>LOGIN</div>}/>*/}
+            {/*    <Route path={'/profile'} element={(*/}
+            {/*        <div>*/}
+            {/*            PROFILE*/}
+            {/*            <Outlet/>*/}
+            {/*        </div>*/}
+            {/*    )}>*/}
+            {/*        <Route path={'*'} element={<div>profile page not found</div>}/>*/}
+            {/*        <Route path={'settings'} element={<div>SETTINGS</div>}/>*/}
+            {/*    </Route>*/}
+            {/*</Routes>*/}
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/*'} element={<div>404</div>}/>*/}
+            {/*    <Route path={'/'} element={<div>MAIN</div>}/>*/}
+            {/*    <Route path={'/login/*'} element={<div>LOGIN</div>}/>*/}
+            {/*    <Route path={'/profile'} element={(*/}
+            {/*        <div>*/}
+            {/*            PROFILE*/}
+            {/*            <Outlet/>*/}
+            {/*        </div>*/}
+            {/*    )}>*/}
+            {/*        <Route path={'settings'} element={<div>SETTINGS</div>}/>*/}
+            {/*    </Route>*/}
+            {/*</Routes>*/}
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/*'} element={<div>404</div>}/>*/}
+            {/*    <Route path={'/'} element={<div>MAIN</div>}/>*/}
+            {/*    <Route path={'/login/*'} element={<div>LOGIN</div>}/>*/}
+            {/*    <Route path={'/profile'} element={(*/}
+            {/*        <div>*/}
+            {/*            PROFILE*/}
+            {/*            <Outlet/>*/}
+            {/*        </div>*/}
+            {/*    )}>*/}
+            {/*        <Route path={'/profile/settings'} element={<div>SETTINGS</div>}/>*/}
+            {/*    </Route>*/}
+            {/*</Routes>*/}
 
             {/*<Routes>*/}
             {/*    <Route path={'/*'} element={<div>404</div>}/>*/}
